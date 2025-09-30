40cb2fb19d2c5a67e21ed3e3db68004113f081ce
AdyeHengker <adye24si@mahasiswa.pcr.ac.id>
2025-09-30 10:21:19 +0700

    Penambahan form pertanyaan

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
-use App\Http\Controllers\PegawaiController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -37,3 +38,6 @@ Route::get('/about', function () {
 Route::get('/home', [HomeController::class, 'index']);
 
 Route::get('/pegawai', [PegawaiController::class, 'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
